e6581fb1503d57a60654822a71f3605168b07d2e
kenneditodd <kenneditodd@gmail.com>
2024-09-27 15:13:22 -0500

    add cellbender

--- E3_E4_pilot/scripts/01_Cell_Ranger/06_cellbender.sh
+++ E3_E4_pilot/scripts/01_Cell_Ranger/06_cellbender.sh
@@ -1,37 +1,37 @@
 #!/bin/sh
-#SBATCH --job-name cellbender       # Name of the job
-#SBATCH --mem 50G                   # Amount of memory allocated for the job
+#SBATCH --job-name=cellbender       # Name of the job
+#SBATCH --mem=50G                    # Amount of memory allocated for the job
 #SBATCH --output logs/%x.%j.stdout  # File for standard output
-#SBATCH --error logs/%x.%j.stderr   # File for standard error output
-#SBATCH --partition gpu             # Specifies the partition (queue)
-#SBATCH --time 08:00:00             # Maximum time the job is allowed to run, HH:MM:SS
-#SBATCH --gres=gpu:1                # Request 1 GPU
+#SBATCH --error=logs/%x.%j.stderr   # File for standard error output
+#SBATCH --partition=gpu             # Specifies the partition (queue)
+#SBATCH --gres=gpu:1                # Specify GPU
+#SBATCH --cpus-per-task=16          # Number of CPU cores per task
+#SBATCH --time=24:00:00             # Maximum time the job is allowed to run, HH:MM:SS
 
-# change to top level of project directory
-cd ../../
-
-# source settings and environment variables
+# source settings
 source $HOME/.bash_profile
-source refs/.env
+source ../../refs/.env
+
+# activate conda environment
+conda activate cellbender
 
 # cellbender version
 cellbender --version
 
-# print sample passed from 03_sample_loop.sh script
+# print sample passed from sample_loop.sh script
 SAMPLE=$1
 echo "sample: $SAMPLE"
 
-# check cuda
-python -c "import torch; print(torch.cuda.is_available())"
+# in order for ckpt.tar.gz in cellbender to save correctly, go to where data is located
+cd ${PROJECT_DIR}/counts/${SAMPLE}/outs
 
 # run cellbender remove-background
 cellbender remove-background \
-      --cuda \
-      --input counts/$SAMPLE/outs/raw_feature_bc_matrix.h5 \
-      --output cellbender/$SAMPLE_cellbender.h5 \
-      --expected-cells 5000
-      
+      --input raw_feature_bc_matrix.h5 \
+      --output ${SAMPLE}_cellbender.h5 \
+      --cuda
+
 # Key
-# --input un-filtered data file, .h5 or matrix directory from 10x
-# --output output file location containing .h5 extension
-# --cude will run on reference GPU
+# --input       :un-filtered data file, .h5 or matrix directory from 10x
+# --output      :output file location containing .h5 extension
+# --cuda        :for use with GPU
