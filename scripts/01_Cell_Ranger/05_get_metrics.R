f137f7c28389cdf9e02174a3be7a5268918f2ce6
kenneditodd <kenneditodd@gmail.com>
2024-12-06 13:33:11 -0600

    minor changes

--- scripts/01_Cell_Ranger/05_get_metrics.R
+++ scripts/01_Cell_Ranger/05_get_metrics.R
@@ -1,26 +1,17 @@
 # load libraries
 library(stringr)
+library(dotenv)
+
+# load the environment variables
+load_dot_env(file = "../../refs/.env")
+
+# access the environment variables
+project_dir <- Sys.getenv("PROJECT_DIR")
 
 # file locations
-locations <- c("/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E3CF1/outs/metrics_summary.csv",
-               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E3CF2/outs/metrics_summary.csv",
-               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E3CM1/outs/metrics_summary.csv",
-               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E3CM2/outs/metrics_summary.csv",
-               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E3PF1/outs/metrics_summary.csv",
-               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E3PF2/outs/metrics_summary.csv",
-               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E3PM1/outs/metrics_summary.csv",
-               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E3PM2/outs/metrics_summary.csv",
-               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E4CF1/outs/metrics_summary.csv",
-               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E4CF2/outs/metrics_summary.csv",
-               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E4CM1/outs/metrics_summary.csv",
-               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E4CM2/outs/metrics_summary.csv",
-               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E4PF1/outs/metrics_summary.csv",
-               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E4PF2/outs/metrics_summary.csv",
-               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E4PM1/outs/metrics_summary.csv",
-               "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/E4PM2/outs/metrics_summary.csv")
-
-# sample names
-names <- str_match(locations, "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/(.+)/outs/metrics_summary.csv")[,2]
+samples <- c("E3CF1","E3CF2","E3CM1","E3CM2","E3PF1","E3PF2","E3PM1","E3PM2",
+             "E4CF1","E4CF2","E4CM1","E4CM2","E4PF1","E4PF2","E4PM1","E4PM2")
+locations <- paste0(project_dir, "/counts/", samples, "/outs/metrics_summary.csv")
 
 # initialize df and loop through files
 df <- data.frame()
@@ -33,18 +24,16 @@ for (i in 1:length(locations)) {
   }
 }
 
-rownames(df) <- names
+rownames(df) <- samples
 c.names <- c("estimated_cells", "mean_reads", "median_genes", "number_reads",
-                  "valid_barcodes", "sequencing_saturation", "Q30_bases_barcode",
-                  "Q30_bases_read", "Q30_bases_UMI", "reads_mapped_genome", "confident_reads_mapped_genome",
-                  "confident_intergenic_reads_mapped", "confident_intronic_reads_mapped",
-                  "confident_exonic_reads_mapped", "confident_reads_mapped_transcriptome",
-                  "reads_mapped_antisense", "fraction_reads", "total_genes", "median_UMI")
-colnames(df) <- c.names
+             "valid_barcodes", "sequencing_saturation", "Q30_bases_barcode",
+             "Q30_bases_read", "Q30_bases_UMI", "reads_mapped_genome", "confident_reads_mapped_genome",
+             "confident_intergenic_reads_mapped", "confident_intronic_reads_mapped",
+             "confident_exonic_reads_mapped", "confident_reads_mapped_transcriptome",
+             "reads_mapped_antisense", "fraction_reads", "total_genes", "median_UMI")
+colnames(df) <- tolower(gsub("\\.", "_", colnames(df)))
 
 write.table(df, 
-            "/research/labs/neurology/fryer/m214960/PLX5622_mice/counts/web_summaries/overall_metrics.tsv",
+            paste0(project_dir, "/counts/web_summaries/overall_metrics.tsv"),
             sep = "\t",
             quote = FALSE)
-
-
---
+++ scripts/01_Cell_Ranger/08_copy_MEX.R
@@ -0,0 +1,62 @@
+# load libraries
+library(dotenv)
+library(DropletUtils)
+library(Seurat)
+library(scCustomize)
+
+# source environment variables
+load_dot_env(file = "../../refs/.env")
+project_dir <- Sys.getenv("PROJECT_DIR")
+
+# read list of samples
+samples <- readLines(paste0(project_dir, "/refs/sample_names.txt"))
+
+###################### Copy PIPseeker MEX files over ###########################
+
+# loop through samples and copy matrix files to new folder
+for (i in samples) {
+  # print the sample you're on
+  print(i)
+
+  # set paths and dir names
+  input_dir <- paste0(project_dir, "/counts/", i, "/outs/filtered_feature_bc_matrix")
+  output_base_dir <- paste0(project_dir, "/matrices")
+  output_new_dir_name <- i
+  
+  # copy barcodes
+  file.copy(from = paste0(input_dir, "/barcodes.tsv.gz"), to = output_base_dir)
+  file.rename(file.path(output_base_dir, "barcodes.tsv.gz"), 
+              paste0(output_base_dir, "/", i, "_barcodes.tsv.gz"))
+  
+  # copy genes
+  file.copy(from = paste0(input_dir, "/features.tsv.gz"), to = output_base_dir)
+  file.rename(file.path(output_base_dir, "features.tsv.gz"), 
+              paste0(output_base_dir, "/", i, "_features.tsv.gz"))
+  
+  # copy matrix
+  file.copy(from = paste0(input_dir, "/matrix.mtx.gz"), to = output_base_dir)
+  file.rename(file.path(output_base_dir, "matrix.mtx.gz"), 
+              paste0(output_base_dir, "/", i, "_matrix.mtx.gz"))
+  
+}
+
+###################### Reformat Cellbender to MEX files ########################
+
+# loop through samples, read cellbender output, reformat, and copy to new folder
+
+#for (i in samples) {
+#  # print the sample you're on
+#  print(i)
+  
+  # set paths and dir names
+#  input_path <- paste0(project_dir, "/counts/",i, "/", i, "_cellbender_filtered.h5")
+#  output_dir <- 
+#    paste0(project_dir, "/matrices/pipseeker_filtered_matrix_sensitivity5_with_cellbender")
+  
+  # read h5 file in and reformat
+#  mat <- Read_CellBender_h5_Mat(input_path)
+#  write10xCounts(path = paste0(output_dir, "/", i),
+#                 x = mat)
+#}
+
+
